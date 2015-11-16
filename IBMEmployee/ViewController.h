eacbad615193648e7b9e2a97a88dfc2ac8c9c44f
Eliran BenIshay <eliran@us.ibm.com>
2015-11-16 17:25:07 -0500

    IBM Employee App - cached data

--- IBMEmployee/ViewController.h
+++ IBMEmployee/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
+@property (strong, nonatomic) IBOutlet UITextField *username;
+
+@property (strong, nonatomic) IBOutlet UITextField *password;
+- (IBAction)loginBtn:(id)sender;
 
 @end
 
--- IBMEmployee/ViewController.m
+++ IBMEmployee/ViewController.m
@@ -14,9 +14,23 @@
 
 @implementation ViewController
 
+@synthesize username;
+@synthesize password;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    username.delegate = self;
+    password.delegate = self;
+    password.secureTextEntry = YES;
+
+    
+    [self.navigationController setNavigationBarHidden:YES];
+    [self.navigationController setTitle:@"Login"];
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +38,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [username resignFirstResponder];
+    [password resignFirstResponder];
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField{
+    if(textField){
+        [textField resignFirstResponder];
+    }
+    return NO;
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [self.navigationController setNavigationBarHidden:YES];
+}
+
+
+- (IBAction)loginBtn:(id)sender {
+}
 @end
