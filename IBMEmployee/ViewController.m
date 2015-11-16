5fa02b3437e9a8e386a347c105fcd2124ead649d
Eliran BenIshay <eliran@us.ibm.com>
2015-11-16 12:44:29 -0500

    Initial Commit

---
+++ IBMEmployee/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IBMEmployee
+//
+//  Created by Eliran BenIshay on 11/16/15.
+//  Copyright © 2015 Eliran BenIshay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
